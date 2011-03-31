09b21e3d839ef7a55ba2a800a4702005662980e5
Vlad <vlad_micutul@yahoo.com>
2011-03-31 14:03:08 +0300

    Added Romanian translation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,33 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad.ghita@xandergroup.ro',
+			'website' => ''
+		),
+		'release-date' => '2011-03-31'
+	);
+
+	/**
+	 * Custom Preferences
+	 */
+	$dictionary = array(
+	
+		'<code>Custom Preferences</code> depends on both <code>%s</code> and <code>%s</code>. Make sure you have these extension installed and enabled.' => 
+		'Extensia <code>Custom Preferences</code> depinde atât de <code>%s</code> cât şi de <code>%s</code>. Asiguraţi-vă că aceste extensii sunt instalate şi activate.',
+
+		'Static Section' => 
+		'Static Section',
+
+		'Publish Tabs' => 
+		'Publish Tabs',
+
+		'It looks like no sections are available that are both static and hidden. <a href="%s">Click here to create some.</a>' => 
+		'Se pare că nu există secţiuni disponibile care să fie şi statice şi ascunse. <a href="%s">Clic aici pentru a crea una.</a>',
+
+		'It looks like your static section has no fields. <a href="%s">Click here to create some.</a>' => 
+		'Se pare că secţiunea dumneavoastră nu conţine câmpuri. <a href="%s">Clic aici pentru a adăuga unul.</a>',
+
+	);
